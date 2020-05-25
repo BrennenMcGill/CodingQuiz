00a347dc7f050fc34cca882f7d9592bd7c57df1f
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-05-24 20:51:48 -0600

    finished main game portion

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,47 +1,49 @@
 var timerEl = document.getElementById("timer");
 var bodyEl = document.getElementById("main");
-var waitingForAnswer = false;
+var waitingForAnswer = false;   
 var questionArray = [
     {
-        q : "Commonly used data types DO NOT include:",
-        a : ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-        c : 2
+        question: "Commonly used data types DO NOT include:",
+        answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        correct: 2
     },
     {
-        q : "The condition in an if / else statement is enclosed with ________.",
-        a : ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
-        c : 2
+        question: "The condition in an if / else statement is enclosed with ________.",
+        answers: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
+        correct: 2
     },
     {
-        q : "Arrays in JavaScript can be used  to store _________.",
-        a : ["1. Numbers and Stings", "2. Other Arrays", "3. Booleans", "4. All Of The Above"],
-        c : 3
+        question: "Arrays in JavaScript can be used  to store _________.",
+        answers: ["1. Numbers and Stings", "2. Other Arrays", "3. Booleans", "4. All Of The Above"],
+        correct: 3
     },
     {
-        q : "Sting values must be enclosed within ______ when being assigned to variables.",
-        a : ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"],
-        c : 2
+        question: "Sting values must be enclosed within ______ when being assigned to variables.",
+        answers: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"],
+        correct: 2
     },
     {
-        q : "A very useful tool used during development and debugging for printing content to the debugger is:",
-        a : ["1. JavaScript", "2. Terminal/Bash", "3. For Loops", "4. Console.log"],
-        c : 3
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. JavaScript", "2. Terminal/Bash", "3. For Loops", "4. Console.log"],
+        correct: 3
     }
 ];
-
+var singleAnswerChosen = false;
+var timeInterval = 0;
 //startTimer function
+var timeLeft = 0;
+
 
 var timer = function(){
-    var timeLeft = 76;
-
-    var timeInterval = setInterval(function(){
+        timeInterval = setInterval(function(){
         timeLeft--;
-        timerEl.textContent = timeLeft;
+        timerEl.textContent = "Time: " + timeLeft;
 
-        if (timeLeft === 0) {
-            clearInterval(timeInterval);
+        if (timeLeft <= 0) {
+            timerEl.textContent = 0;
+            timeLeft = 0;
     
-            // call end game function
+            endGame();
         }
 
         if (timeLeft === 69) {
@@ -53,6 +55,7 @@ var timer = function(){
 
 //displayBeginning function
 var displayBeginning = function() {
+    bodyEl.innerHTML = "";
     var titleEl = document.createElement("h1");
     titleEl.textContent = "Coding Quiz"
     titleEl.className= "question";
@@ -71,10 +74,16 @@ var displayBeginning = function() {
 
     startQuizEl.addEventListener("click", startGame);
     bodyEl.appendChild(startQuizEl);
+
 }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 //startGame function
-var startGame = function() {
+async function startGame() {
+    timeLeft = 76;
+
     timer();
     // for loop that runs through question[object]
     for (var i=0; i < questionArray.length; i++) {
@@ -84,7 +93,7 @@ var startGame = function() {
         waitingForAnswer = true;
         // wait for waitingForAnswer to become false in while loop
         while (waitingForAnswer) {
-
+            await sleep(1000);
         }
     }
     // when for loop is done call endGame()
@@ -92,24 +101,96 @@ var startGame = function() {
 }
 
 //displayNewQuestion function
-var displayNewQuestion = function() {
-    // inputs = question object
-    // generate question
-    // generate ul for answers
-    // generate li elements
-    // make all li elements clickable and call chooseanser(question object, answer index)
+var displayNewQuestion = function(questionObj) {
+    bodyEl.innerHTML = "";
+    var questionEl = document.createElement("h1");
+    questionEl.textContent = questionObj.question;
+    questionEl.className = "question";
 
+    bodyEl.appendChild(questionEl);
+
+    var answerEl = document.createElement("ul");
+
+    bodyEl.appendChild(answerEl);
+
+    for (var i = 0; i < questionObj.answers.length; i++) {
+        var answerList = document.createElement("li");
+        answerList.textContent = questionObj.answers[i];
+        answerList.className = "button";
+        var isCorrect = (questionObj.correct === i);
+        answerList.addEventListener("click", chooseAnswer.bind(null, questionObj, i));
+        answerEl.appendChild(answerList);
+    }
+    singleAnswerChosen = false;
 }
 
-//displayAfterAnswer function
-    // input = correct
-    // generate right or wrong element based on input
-    // start timer to call nextQuestion()
+var displayAfterAnswer = function(isCorrect) {
+ // generate right or wrong element based on input
+    if (isCorrect) {
+        var correctEl = document.createElement("h2");
+        correctEl.className = "after-answer";
+        correctEl.textContent = "Correct!!";
 
-//nextQuestion 
+        bodyEl.appendChild(correctEl);
+    }
+
+    else {
+        var wrongEl = document.createElement("h2");
+        wrongEl.className = "after-answer";
+        wrongEl.textContent = "Wrong!!";
+
+        bodyEl.appendChild(wrongEl);
+    }
+    // start timer to call nextQuestion()
+    setTimeout(nextQuestion, 2000);
+}
+
+
+var nextQuestion = function() {
+    waitingForAnswer = false;
+}
     // switch waitingForAnswer to false
 
 //displayHighScoreEntry function
+var displayHighScoreEntry = function() {
+    bodyEl.innerHTML = "";
+    var allDoneEl = document.createElement("h1");
+    allDoneEl.textContent = "All Done!";
+    allDoneEl.className = "question"
+
+    bodyEl.appendChild(allDoneEl);
+
+    var finalScoreEl = document.createElement("p");
+    finalScoreEl.textContent = "Your final Score is " + timeLeft;
+    finalScoreEl.className = "instructions";
+
+    bodyEl.appendChild(finalScoreEl);
+
+    var formContainerEl = document.createElement("div");
+    formContainerEl.className = "form-container";
+
+    bodyEl.appendChild(formContainerEl);
+    
+    var enterInitialEl = document.createElement("p");
+    enterInitialEl.textContent = "Enter your intials to put on high score list!";
+    enterInitialEl.className = "instructions";
+
+    formContainerEl.appendChild(enterInitialEl);
+
+    var formInputEl = document.createElement("INPUT");
+    formInputEl.setAttribute("placeholder", "Intials");
+    formInputEl.className = "form-input";
+
+    formContainerEl.appendChild(formInputEl);
+
+    var submitButtonEl = document.createElement("button");
+    submitButtonEl.textContent = "Submit";
+    submitButtonEl.className = "submit-button";
+
+    submitButtonEl.addEventListener("click", displayBeginning);
+    formContainerEl.appendChild(submitButtonEl);
+    
+}
     // displays all done element
     // displays your final score = time left
     // displays form for initial with submit button
@@ -122,14 +203,25 @@ var displayNewQuestion = function() {
     // generate button to startGame()
     // generate button to clear high scores and call clearHighScores()
     
-// endgame function
-    // call displayHighScoreEntry()
+var endGame = function(){
+    timerEl.textContent = "";
+    clearInterval(timeInterval);
+    displayHighScoreEntry();
+}
 
 //chooseAnswer function
-    // inputs = question object, answer index
+var chooseAnswer = function(questionObj, i) {
+    if (singleAnswerChosen === false){
     // check if question object selected correct index 
-    // if incorrect timer - 10
+    var isCorrect = (questionObj.correct === i)
+    if (!isCorrect) {
+    timeLeft = timeLeft - 15;
+    }
+    singleAnswerChosen = true;
     // calls displayAfterAnswer(whether its true or not)
+    displayAfterAnswer(isCorrect);
+}
+}
 
 //highScoreSubmit function
     // inputs(initial + timers)
@@ -140,9 +232,5 @@ var displayNewQuestion = function() {
     // prompt "are you sure?"
     // clear local.storage if prompt is true and alert "scores cleared"
 
-//object for questions
-    // question string
-    // answer string array
-    // correct idex of answer string array
 
-    displayBeginning();
+displayBeginning();
